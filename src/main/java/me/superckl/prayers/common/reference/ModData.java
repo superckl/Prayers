5917be63bf4cf6deb78a34810288a15fcb0f184d
superckl <lars4647@umn.edu>
2014-12-30 00:43:02 -0800

    Messing with the build script to automate version numbers a bit more

--- src/main/java/me/superckl/prayers/common/reference/ModData.java
+++ src/main/java/me/superckl/prayers/common/reference/ModData.java
@@ -6,7 +6,7 @@ public final class ModData {
 
 	public static final String MOD_ID = "Prayers";
 	public static final String MOD_NAME= "Prayers";
-	public static final String VERSION = "0.1-Beta";
+	public static final String VERSION = "@VERSION@";
 	public static final String GUI_FACTORY = "me.superckl.prayers.common.gui.GuiFactory";
 	public static final String CLIENT_PROXY = "me.superckl.prayers.proxy.ClientProxy";
 	public static final String SERVER_PROXY = "me.superckl.prayers.proxy.ServerProxy";
